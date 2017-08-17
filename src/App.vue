4753078c1ff9af7a02e8db207ebc002192bffe0e
stepanowon <stepanowon@hotmail.com>
2017-08-17 10:20:36 +0900

    final

--- src/App.vue
+++ src/App.vue
@@ -34,4 +34,16 @@ export default {
   max-width: 800px;
   margin:  0 auto;
 }
+.Chart {
+  background: #212733;
+  border-radius: 15px;
+  box-shadow: 0px 2px 15px rgba(25, 25, 25, 0.27);
+  margin:  25px 0;
+}
+.Chart h2 {
+  margin-top: 0;
+  padding: 15px 0;
+  color:  rgba(255, 0,0, 0.5);
+  border-bottom: 1px solid #323d54;
+}
 </style>
